ec970a5d5de585914e69112291e0eda0552581d3
home <mokhtari.sahar1380@gmail.com>
2021-06-05 22:36:53 +0430

    finished login and signup

---
+++ app/src/main/java/ir/shariaty/financialmanagement/create_an_account.java
@@ -0,0 +1,14 @@
+package ir.shariaty.financialmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class create_an_account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_an_account);
+    }
+}
